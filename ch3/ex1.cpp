f12638d9fb0e4f92b59ac51339f4e5211b493abb
Noah Kleedtke <nak4adventure@Noahs-MBP.attlocal.net>
2020-05-28 14:37:34 -0400

    modified ex1

---
+++ ch3/ex1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter Height (in): __\b\b";
+    int i;
+    cin >> i;
+    cout << "Height: " << i << endl;
+    return 0;
+}
