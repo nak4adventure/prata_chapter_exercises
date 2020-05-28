171521a37561d1d196f4560b63c97b739916c434
Noah Kleedtke <nak4adventure@Noahs-MBP.attlocal.net>
2020-05-28 14:38:54 -0400

    new update

--- ch2/ex1.cpp
+++ ch2/ex1.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 int main() 
 {
-   cout << "Name: Noah" << endl;
-   cout << "Address: 12440" << endl;
-   return 0;
-
+    cout << "Name: Noah" << endl;
+    cout << "Address: 12440" << endl;
+    return 0;
 }
